2602bfedfefbb70cec887b99451d193fee6bd842
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-10-31 17:33:10 +0700

    build a title of site

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
