ff0b742119fffbdf9bc5e2f34ae5e740a5c9ec7a
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-10-31 17:30:57 +0700

    add firebase service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,48 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+// Firebase service
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+// Auth service
+import { AuthenticationService } from '../app/services/authentication.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginFormComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
+  entryComponents: [LoginFormComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
