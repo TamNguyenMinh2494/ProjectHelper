7ad2b621bbd626672b8edd557a8700d42471761c
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-10-31 17:29:00 +0700

    add firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD07uGaOcO4HhTlpL3K6D9WQr7wAaa1lsQ",
+    authDomain: "uploadfile-85eef.firebaseapp.com",
+    databaseURL: "https://uploadfile-85eef.firebaseio.com",
+    projectId: "uploadfile-85eef",
+    storageBucket: "uploadfile-85eef.appspot.com",
+    messagingSenderId: "724239810594",
+    appId: "1:724239810594:web:453ea2ef3f52d145"
+  }
 };
 
 /*
