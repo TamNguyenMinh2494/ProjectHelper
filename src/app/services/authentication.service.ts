b7a886fc9c6651c5b22cf5f2552cbd256ec0ec08
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-01 16:01:22 +0700

    removed function in constructor

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -9,31 +9,15 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class AuthenticationService {
 
-  // private userAuth: any;
-  islogged = false;
-
   constructor(
     public angularFireAuth: AngularFireAuth,
     public router: Router
-  ) {
-    this.angularFireAuth.authState.subscribe(userResponse => {
-      if (userResponse) {
-        // this.userAuth = userResponse;
-        this.islogged = true;
-        localStorage.setItem('user', JSON.stringify(userResponse));
-      } else {
-        localStorage.setItem('user', null);
-        this.islogged = false;
-      }
-    });
-  }
+  ) { }
 
   async loginWithGoogle() {
     return await this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
   }
   isUserLoggedin() {
     return JSON.parse(localStorage.getItem('user'));
-    // JSON.parse(localStorage.getItem('displayName'));
-    // JSON.parse(localStorage.getItem('photoURL'));
   }
 }
