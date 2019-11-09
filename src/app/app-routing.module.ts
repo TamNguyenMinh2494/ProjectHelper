1c06da1ffd631d2b624604ed1969b6bc9040ccba
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-09 22:02:49 +0700

    add CreateAProjectComponent route path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { CreateAProjectComponent } from './create-aproject/create-aproject.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'createAProject', component: NavigationComponent },
+  { path: 'createAProject', component: CreateAProjectComponent },
   { path: '**', redirectTo: '' },
 
 ];
