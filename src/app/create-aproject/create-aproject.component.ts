95d0620ba9fadf676d132278606fd8cea8ab6672
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-11 12:20:03 +0700

    add form to create

--- src/app/create-aproject/create-aproject.component.ts
+++ src/app/create-aproject/create-aproject.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAProjectComponent implements OnInit {
 
+
+  jobsTitle = ['Website', 'Application', 'Design', 'Video', 'Marketing'];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  formatLabel(value: number) {
+    if (value < 1000000) {
+      return Math.round(value / 1000) + 'k';
+    } else {
+      return Math.round(value / 1000000) + 'm';
+    }
+    return value;
+  }
+  // tslint:disable-next-line: member-ordering
+  max = 10000000;
+  // tslint:disable-next-line: member-ordering
+  min = 100000;
+  // tslint:disable-next-line: member-ordering
+  step = 20000;
+  // tslint:disable-next-line: member-ordering
+  thumbLabel = false;
+  value = null;
 
 }
