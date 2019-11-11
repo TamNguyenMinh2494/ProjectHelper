e779aa4b43113990168560d5cf89affa2fee0fa2
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-11 10:29:08 +0700

    fix routing

---
+++ src/app/create-aproject/create-aproject.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateAProjectComponent } from './create-aproject.component';
+
+describe('CreateAProjectComponent', () => {
+  let component: CreateAProjectComponent;
+  let fixture: ComponentFixture<CreateAProjectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CreateAProjectComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateAProjectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/create-aproject/create-aproject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-aproject',
+  templateUrl: './create-aproject.component.html',
+  styleUrls: ['./create-aproject.component.scss']
+})
+export class CreateAProjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
