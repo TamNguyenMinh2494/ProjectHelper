e5509fe655ea06dc57e856d44b1adb31bdcce9e4
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-12 19:39:01 +0700

    add firebaseConfig to ng build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD07uGaOcO4HhTlpL3K6D9WQr7wAaa1lsQ",
+    authDomain: "uploadfile-85eef.firebaseapp.com",
+    databaseURL: "https://uploadfile-85eef.firebaseio.com",
+    projectId: "uploadfile-85eef",
+    storageBucket: "uploadfile-85eef.appspot.com",
+    messagingSenderId: "724239810594",
+    appId: "1:724239810594:web:453ea2ef3f52d145"
+  }
 };
