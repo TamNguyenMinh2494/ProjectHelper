f3af460200df525f430b862157d1c8e4fe13945c
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-12 19:36:05 +0700

    created form for create a project

--- src/app/create-aproject/create-aproject.component.ts
+++ src/app/create-aproject/create-aproject.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Requirement } from '../model/requirement';
+import { CreateAProjectService } from '../services/create-aproject.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-aproject',
   templateUrl: './create-aproject.component.html',
@@ -7,31 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateAProjectComponent implements OnInit {
 
+  requirement: Requirement = new Requirement();
+  submitted = false;
+  value = '';
 
-  jobsTitle = ['Website', 'Application', 'Design', 'Video', 'Marketing'];
-
-  constructor() { }
+  constructor(private requirementService: CreateAProjectService, private afAuth: AngularFireAuth, private router: Router) { }
 
   ngOnInit() {
+
   }
 
-  formatLabel(value: number) {
-    if (value < 1000000) {
-      return Math.round(value / 1000) + 'k';
-    } else {
-      return Math.round(value / 1000000) + 'm';
+  newRequirement(): void {
+    this.submitted = false;
+    this.requirement = new Requirement();
+  }
+
+  save() {
+    this.requirement.ownerId = this.afAuth.auth.currentUser.uid;
+    this.requirementService.createRequirement(this.requirement);
+    this.requirement = new Requirement();
+  }
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+    this.router.navigate(['']);
+  }
+
+  price = 0;
+  jobsTitle = ['Website', 'Application', 'Design', 'Video', 'Marketing'];
+  formatLabel(price: number) {
+    if (price < 1000000) {
+      return Math.round(price / 1000) + 'k';
     }
-    return value;
+    if (price >= 1000000) {
+      return Math.round(price / 1000000) + 'm';
+    }
+    return price;
   }
   // tslint:disable-next-line: member-ordering
   max = 10000000;
   // tslint:disable-next-line: member-ordering
   min = 100000;
   // tslint:disable-next-line: member-ordering
-  step = 20000;
+  step = 200000;
   // tslint:disable-next-line: member-ordering
   thumbLabel = false;
 
-  value = null;
+
+
+
 
 }
