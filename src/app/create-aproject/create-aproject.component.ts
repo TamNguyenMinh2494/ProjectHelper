a202543b13f9104e0c79715d648830fc1d7be9bb
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-22 12:28:55 +0700

    fixed Price's options

--- src/app/create-aproject/create-aproject.component.ts
+++ src/app/create-aproject/create-aproject.component.ts
@@ -3,6 +3,8 @@ import { Requirement } from '../model/requirement';
 import { CreateAProjectService } from '../services/create-aproject.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-create-aproject',
   templateUrl: './create-aproject.component.html',
@@ -13,8 +15,14 @@ export class CreateAProjectComponent implements OnInit {
   requirement: Requirement = new Requirement();
   submitted = false;
   value = '';
-
-  constructor(private requirementService: CreateAProjectService, private afAuth: AngularFireAuth, private router: Router) { }
+  Price = ['0 - 1,000,000 VNĐ', '1,000,000 - 5,000,000 VNĐ', '5,000,000 - 10,000,000 VNĐ', '15,000,000 - 20,000,000 VNĐ'];
+  jobsTitle = ['Website', 'Application', 'Design', 'Video', 'Marketing'];
+  durationInSeconds = 5;
+  constructor(
+    private requirementService: CreateAProjectService,
+    private afAuth: AngularFireAuth,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
 
@@ -34,30 +42,25 @@ export class CreateAProjectComponent implements OnInit {
     this.submitted = true;
     this.save();
     this.router.navigate(['']);
+
   }
-
-  price = 0;
-  jobsTitle = ['Website', 'Application', 'Design', 'Video', 'Marketing'];
-  formatLabel(price: number) {
-    if (price < 1000000) {
-      return Math.round(price / 1000) + 'k';
-    }
-    if (price >= 1000000) {
-      return Math.round(price / 1000000) + 'm';
-    }
-    return price;
-  }
-  // tslint:disable-next-line: member-ordering
-  max = 10000000;
-  // tslint:disable-next-line: member-ordering
-  min = 100000;
-  // tslint:disable-next-line: member-ordering
-  step = 200000;
-  // tslint:disable-next-line: member-ordering
-  thumbLabel = false;
-
-
-
-
 
 }
+  // formatLabel(price: number) {
+  //   if (price < 1000000) {
+  //     return Math.round(price / 1000) + 'k';
+  //   }
+  //   if (price >= 1000000) {
+  //     return Math.round(price / 1000000) + 'm';
+  //   }
+  //   return price;
+  // }
+  // tslint:disable-next-line: member-ordering
+  // max = 10000000;
+  // // tslint:disable-next-line: member-ordering
+  // min = 100000;
+  // // tslint:disable-next-line: member-ordering
+  // step = 200000;
+  // // tslint:disable-next-line: member-ordering
+  // thumbLabel = false;
+
