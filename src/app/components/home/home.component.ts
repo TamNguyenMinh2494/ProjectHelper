f5001d4cdff60aad096626760e8ca09316c39314
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-25 23:31:19 +0700

    add edit and delete button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { CreateAProjectService } from '../../services/create-aproject.service';
-import { map } from 'rxjs/operators';
-import { Requirement } from '../../model/requirement';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { pipe } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-home',
@@ -13,23 +12,32 @@ import { pipe } from 'rxjs';
 export class HomeComponent implements OnInit {
 
   requirements: any;
+  displayedColumns: string[] = ['Title', 'Type', 'Price', 'Description', 'Actions'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private requirementService: CreateAProjectService) { }
 
   ngOnInit() {
     this.requirementService.getAllRequirements().subscribe(res => this.dataSource = new MatTableDataSource(res));
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
-
-  displayedColumns: string[] = ['Title', 'Type', 'Price', 'Description'];
-
-  dataSource = new MatTableDataSource();
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   checkBox = ['All', 'Website', 'Application', 'Design', 'Video', 'Marketing'];
 
-
-
+  onUpdate(element) {
+    this.requirementService.selected = element;
+  }
+  onDelete(key: string) {
+    this.requirementService.deleteRequirement(key);
+  }
 }
