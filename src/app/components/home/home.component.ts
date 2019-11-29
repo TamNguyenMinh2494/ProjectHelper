cca2e5cec0eca37aa678d297a38df5b8b93f6f04
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-29 18:24:47 +0700

    fixed sort, paginator and build dialog

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 import { CreateAProjectService } from '../../services/create-aproject.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { error } from 'util';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { ReqUpdateFormComponent } from '../req-update-form/req-update-form.component';
+import { Requirement } from 'src/app/model/requirement';
 
 @Component({
   selector: 'app-home',
@@ -13,17 +17,30 @@ export class HomeComponent implements OnInit {
 
   requirements: any;
   displayedColumns: string[] = ['Title', 'Type', 'Price', 'Description', 'Actions'];
-  dataSource = new MatTableDataSource();
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  dataSource: MatTableDataSource<Requirement>;
+  // static = false khi sử dụng ngIf hoặc ngFor, ngược lại static = true khi là tĩnh
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor(private requirementService: CreateAProjectService) { }
+  constructor(
+    private requirementService: CreateAProjectService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
-    this.requirementService.getAllRequirements().subscribe(res => this.dataSource = new MatTableDataSource(res));
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    this.requirementService.getAllRequirements().subscribe(res => {
+      this.dataSource = new MatTableDataSource(res);
+      setTimeout(() => {
+        console.log(this.sort);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      }, 500);
+    });
+
+
   }
+
+
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -34,10 +51,17 @@ export class HomeComponent implements OnInit {
 
   checkBox = ['All', 'Website', 'Application', 'Design', 'Video', 'Marketing'];
 
-  onUpdate(element) {
-    this.requirementService.selected = element;
+
+  onUpdate(element: Requirement) {
+    const dialogRef = this.dialog.open(ReqUpdateFormComponent, { width: '500px', data: element });
+    dialogRef.afterClosed().subscribe((data) => {
+      this.requirementService.updateRequirement(data, (res) => {
+        alert(res.status);
+      });
+    });
   }
   onDelete(key: string) {
     this.requirementService.deleteRequirement(key);
   }
+
 }
