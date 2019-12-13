14946d819d3721a270102461a0639c56d8153ae1
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-12-13 09:05:49 +0700

    connect createRequirement function to Nodejs

--- src/app/services/create-aproject.service.ts
+++ src/app/services/create-aproject.service.ts
@@ -44,8 +44,17 @@ export class CreateAProjectService {
   }
 
   createRequirement(requirement: Requirement): void {
+    this.http.post('http://localhost:3000/project/create', {
+      title: requirement.title,
+      type: requirement.type,
+      price: requirement.price,
+      description: requirement.description,
+      ownerId: requirement.ownerId,
+    }).subscribe((res) => {
+      console.log(res);
+    });
     // tslint:disable-next-line: max-line-length
-    this.db.collection('requirements/').doc((Math.round(new Date().getTime() / 1000) + '-' + Math.floor((1000 + Math.random() * 9999)).toString()).toString()).set({ ...requirement });
+    // this.db.collection('requirements/').doc((Math.round(new Date().getTime() / 1000) + '-' + Math.floor((1000 + Math.random() * 9999)).toString()).toString()).set({ ...requirement });
   }
 
   async updateRequirement(requirement: Requirement, onResult) {
@@ -55,8 +64,9 @@ export class CreateAProjectService {
       type: requirement.type,
       price: requirement.price,
       description: requirement.description,
+      uid: this.afAuth.auth.currentUser.uid, // Xét ownerId hiện tại khớp với id của item cần update thì sẽ hợp lệ
     }).subscribe((res) => {
-      onResult(res);
+      onResult(res); // trả về kết quả và xuất thông báo
     });
     // return await this.requirementRef.doc(requirement.key).update(requirement);
   }
@@ -64,7 +74,7 @@ export class CreateAProjectService {
   deleteRequirement(requirement: Requirement, onResult) {
     console.log(requirement);
     this.http.post('http://localhost:3000/project/delete', {
-      uid: this.afAuth.auth.currentUser.uid,
+      uid: this.afAuth.auth.currentUser.uid, // Xét ownerId hiện tại khớp với id của item cần xóa thì sẽ hợp lệ
       key: requirement.key
     }).subscribe((res) => {
       onResult(res);
